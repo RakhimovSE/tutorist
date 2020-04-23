5a994e911c786e187ae4ceea31bc8519d46c1ef5
rakhimovse <JTzE8RYJeU9x8aw>
2020-04-23 05:01:22 +0300

    Сделал панель навигации

--- routes/index.router.js
+++ routes/index.router.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const helpers = require('./helpers');
 
 router.get('/', async (req, res, next) => {
+  if (req.user) {
+    res.redirect('/dashboard');
+  } else {
+    res.render('index');
+  }
+});
+
+router.get('/dashboard', helpers.ensureAuthenticated, async (req, res, next) => {
   const data = {
     user: req.user,
   };
 
-  res.render('index', data);
+  res.render('dashboard', data);
 });
 
 router.get('/login', helpers.ensureNotAuthenticated, async (req, res, next) => {
