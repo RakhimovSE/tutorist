4db974f7fb28182537b4ae0845d22946ba627ddb
Sevastyan Rakhimov <Rakhimov.SE@gmail.com>
2020-05-02 19:30:54 +0300

    Починил проверку авторизации

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,19 +1,22 @@
 const userController = require('../db/controllers/user.controller');
 
 exports.ensureAuthenticated = async (req, res, next) => {
-  const user = await userController.get(req.user.id);
-  if (user) return next();
+  if (req.isAuthenticated()) {
+    const user = await userController.get(req.user.id);
+    if (user) return next();
+  }
   res.redirect('/login')
 }
 
 exports.ensureAuthenticatedApi = async (req, res, next) => {
-  const user = await userController.get(req.user.id);
-  if (user) return next();
+  if (req.isAuthenticated()) {
+    const user = await userController.get(req.user.id);
+    if (user) return next();
+  }
   res.status(401).json({ error: 'You need to login as a tutor first' });
 }
 
 exports.ensureNotAuthenticated = async (req, res, next) => {
-  const user = await userController.get(req.user.id);
-  if (!user) return next();
+  if (!req.isAuthenticated()) return next();
   res.redirect('/dashboard')
 }
